6349ba2222d7f3039c65fa14ec6565313f287b34
Ayush <ayushkumarp20@gmail.com>
2021-04-21 17:50:14 +0530

    frontend first commit..
    Basic Files for frontend..

---
+++ backend/data/products.js
@@ -0,0 +1,68 @@
+const products = [
+    {
+        _id:'1',
+        name: 'Ikigai: The Japanese Secret to a Long and Happy Life',
+        image: '/images/Ikigai.jpg',
+        description:
+        'The people of Japan believe that everyone has an ikigai a reason to jump out of bed each morning. And according to the residents of the Japanese island of Okinawa – the world’s longest-living people – finding it is the key to a longer and more fulfilled life. Inspiring and comforting, this book will give you the life-changing tools to uncover your personal ikigai. It will show you how to leave urgency behind, find your purpose, nurture friendships and throw yourself into your passions.',
+        authors: 'Francesc Miralles, Hector Garcia',
+        genre:'Self-help Book',
+        price: 300,
+        rating: 4.5,
+        numReviews: 4,
+        countInStock: 3,
+    },
+    {
+        _id:'2',
+        name: 'Moral Compass',
+        image: '/images/mc.jpg',
+        description:
+        'In this compelling novel, Danielle Steel illuminates the dark side of one drunken night, with its tragic consequences, from every possible point of view. As the drama unfolds, the characters will reach a crossroads where they must choose between truth and lies, between what is easy and what is right, and find the moral compass they will need for the rest of their lives.',
+        authors: 'Danielle Steel',
+        genre:'Domestic Fiction',
+        price: 250,
+        rating: 3.9,
+        numReviews: 3,
+        countInStock: 0,
+    },
+    {
+        _id:'3',
+        name: 'Harry Potter and the Prisoner of Azkaban',
+        image: '/images/hp.jpg',
+        description:
+        'For twelve long years, the dread fortress of Azkaban held an infamous prisoner named Sirius Black. Convicted of killing thirteen people with a single curse, he was said to be the heir apparent to the Dark Lord, Voldemort.',
+        authors: 'Danielle Steel',
+        genre:'Fiction, Adventure',
+        price: 500,
+        rating: 4.9,
+        numReviews: 4,
+        countInStock: 15,
+    },
+    {
+        _id:'4',
+        name: 'Imperfections of Memory',
+        image: '/images/mem.jpg',
+        description:
+        'Losing her memory might just be losing her soul and leaving the world behind.',
+        authors: 'Angelena Aludo',
+        genre:'Slice of Life',
+        price: 200,
+        rating: 3.2,
+        numReviews: 9,
+        countInStock: 26,
+    },
+    {
+        _id:'5',
+        name: 'Secret Widow',
+        image: '/images/sw.jpg',
+        description:
+        'Whoever said that time heals all wounds . . . lied.A decade after the death of her husband, Nari Yoon still mourns. On the ten-year anniversary of the worst day of her life, co-worker Lucas Tucker tries to save her from self destruction in a tropical paradise.',
+        authors: 'Sylvie Fox',
+        genre:'Romance novel',
+        price: 250,
+        rating: 3.8,
+        numReviews: 7,
+        countInStock: 20,
+    },
+]
+export default products
---
+++ frontend/src/screens/ProductScreen.js
@@ -0,0 +1,86 @@
+import React, {useState, useEffect} from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button, ListGroupItem } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import axios from 'axios'
+
+const ProductScreen = ({match}) => {
+    const [product, setProduct] = useState({})
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const { data } = await axios.get(`/api/products/${match.params.id}`)
+            
+            setProduct(data)
+            
+        }
+
+        fetchProduct()
+    }, [match])
+
+    return (
+        <>
+          <Link className='btn btn-light my-3'to ='/'>Go Back</Link> 
+          <Row>
+            <Col md={4}>
+                <Image src={product.image} alt={product.name} fluid />
+            </Col>
+            <Col md={5}>
+                <ListGroup variant='flush'>                                                   
+                    <ListGroup.Item>
+                        <h3>{product.name}</h3>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        Author(s): { product.authors}
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        <Rating value={product.rating} text={` ${ product.numReviews} reviews`} />
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        Price: ₹{product.price}
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                        Genre: { product.genre}
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                       <h5> Description:</h5> {product.description}
+                    </ListGroup.Item>
+                </ListGroup>
+            </Col>
+            <Col md={3}>
+                <Card>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <Row>
+                                <Col>
+                                Price:
+                                </Col>
+                                <Col>
+                                <strong>₹{product.price}</strong>
+                                </Col>
+                            </Row>
+                        </ListGroup.Item>
+                    </ListGroup>
+                    <ListGroup variant='flush'>
+                        <ListGroup.Item>
+                            <Row>
+                                <Col>
+                                Status:
+                                </Col>
+                                <Col>
+                                {product.countInStock>0 ? 'In Stock'  : 'Out of Stock'}
+                                </Col>
+                            </Row>
+                        </ListGroup.Item>
+                    </ListGroup>
+                    <Button className='btn-block' type='button' disabled={product.countInStock===0}>
+                        Add to Cart
+                    </Button>   
+                </Card>
+            </Col>
+            </Row>
+        </>
+    )
+}
+
+export default ProductScreen
