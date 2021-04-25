288dbd8c93543124f9539189f05a553e38308fc5
VernieThorpe <verniethorpe@VT-MacBook.local>
2021-04-25 22:43:55 +0530

    My first commit - Checkout steps, Payment Screen and Place order screen added and connected ordered data to database

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,46 @@
-import express from 'express'
-import dotenv  from 'dotenv'
-import colors from 'colors'
-import {notFound, errorHandler} from './middleware/errorMiddleware.js'
-import connectDB from './config/db.js'
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
 
-import productRoutes from './routes/productRoutes.js'
-import userRoutes from './routes/userRoutes.js'
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
-dotenv.config()
+dotenv.config();
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('API is running.....')
-})
+app.get("/", (req, res) => {
+  res.send("API is running.....");
+});
 
-app.use('/api/products', productRoutes)
-app.use('/api/users', userRoutes)
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
-app.use(notFound)
-app.use(errorHandler)
+app.use(notFound);
+app.use(errorHandler);
 
-app.get('/api/products', (req, res) => {
-        res.json(products)
-    })
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(p=> p._id===req.params.id)
-    res.json(product)
-})  
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
 
-const PORT=process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.cyan.bold))
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port ${PORT}`.cyan.bold
+  )
+);
