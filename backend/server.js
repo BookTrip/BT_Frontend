2d0652ff3e796b2fe6405fa33c10d33ba8b0570b
Ayush <ayushkumarp20@gmail.com>
2021-04-27 10:07:43 +0530

    Product reviews

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,16 @@
+<<<<<<< Updated upstream
 import express from 'express'
 import dotenv  from 'dotenv'
 import products from './data/products.js'
+=======
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import morgan from 'morgan'
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
+>>>>>>> Stashed changes
 
 dotenv.config()
 
@@ -14,10 +24,18 @@ app.get('/api/products', (req, res) => {
         res.json(products)
     })
 
+<<<<<<< Updated upstream
 app.get('/api/products/:id', (req, res) => {
     const product = products.find(p=> p._id===req.params.id)
     res.json(product)
 })  
+=======
+if (process.env.NODE_ENV=== 'development') {
+  app.use(morgan('dev'))
+}
+
+app.use(express.json());
+>>>>>>> Stashed changes
 
 const PORT=process.env.PORT || 5000
 
